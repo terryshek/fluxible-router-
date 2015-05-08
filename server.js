c79798f9707bd86506cdebffd2fd835eb96c662e
terryshek <terry_shekhinshing@msn.com>
2015-05-08 23:13:21 +0800

    commit fluxible router

---
+++ server.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2014, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+'use strict';
+require('babel/register');
+var express = require('express');
+//var favicon = require('serve-favicon');
+var serialize = require('serialize-javascript');
+var navigateAction = require('fluxible-router').navigateAction;
+var debug = require('debug')('Example');
+var React = require('react');
+var app = require('./app');
+var HtmlComponent = React.createFactory(require('./components/Html.js'));
+
+var server = express();
+//server.use(favicon(__dirname + '/../favicon.ico'));
+server.use(express.static(__dirname + '/public'));
+
+server.use(function (req, res, next) {
+    var context = app.createContext();
+
+    debug('Executing navigate action');
+    context.executeAction(navigateAction, { url: req.url }, function (err) {
+        if (err) {
+            if (err.statusCode && err.statusCode === 404) {
+                next();
+            } else {
+                next(err);
+            }
+            return;
+        }
+
+        debug('Exposing context state');
+        var exposed = 'window.App=' + serialize(app.dehydrate(context)) + ';';
+
+        debug('Rendering Application component into html');
+        var Component = app.getComponent();
+
+        var html = React.renderToStaticMarkup(HtmlComponent({
+            state: exposed,
+            markup: React.renderToString(context.createElement()),
+            context: context.getComponentContext()
+        }));
+
+        debug('Sending markup');
+        res.send(html);
+    });
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port);
+console.log('Listening on port ' + port);
